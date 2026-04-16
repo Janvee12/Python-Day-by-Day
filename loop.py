9f1b90962d6b34f229134ef13bf30782ee876478
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-16 22:57:10 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,15 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# #same thing in loop we write
+# for i in range(1,6):
+#     print(i)
+
+
+n = int(input("Enter a number: "))
+
+for i in range(1,11):
+    print(f"{n} X {i} = {n*i}")
