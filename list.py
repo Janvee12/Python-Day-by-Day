3415601487fe73043e9a5d3cd2ce13761e946ede
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-19 14:34:37 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+friends = ["Apple","Orange","Banana",5,False]
+
+print(friends[1])
+friends[1] = "Grapes" #In list we have change the word but in string we are not chnge.
+
+print(friends[1])
+
+#slicing of list 
+print(friends[0:4])
+
+friends.append("Janvee") #add karna element list me 
+
+print(friends)
+
+l1 = [3,54,8,26,10]
+#l1.sort()
+#l1.reverse()
+l1.insert(3,65)  #insert 65 value in index 3 
+l1.pop(2)
+print(l1)
+
+
