7c74fe877e1bc2e14c73ff83a80792cd8b99def9
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-20 16:27:53 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,7 @@
+name = "Janvee Sharma"
+a=type(name)
+print(a)
+shortname=name[2:6] #starting form index 2 but we have to include 2 to 5 we have not include index 6 value 
+print(shortname)
+b ="abcdefghijklmnopqrstuvwxyz"
+b[1:9:4]
