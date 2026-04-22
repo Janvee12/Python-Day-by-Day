48e3d35926baf28484c6c84233f150cbd822b0b9
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-22 22:44:32 +0530

    Add files via upload

---
+++ star pattern.py
@@ -0,0 +1,33 @@
+'''
+For n = 3
+     *
+    ***
+   *****
+
+'''
+
+# n = int(input("Enter the number: "))
+# for i in range(1, n+1):
+#     print(""* (n-1),end=" ")
+#     print("*"* (2*i-1),end=" ")
+#     print(" ")
+
+'''
+
+* * *
+*   *       for n = 3
+* * *
+
+
+'''
+n = int(input("Enter the number: "))
+for i in range(1, n+1):
+    if(i==1 or i==n):
+          print("*"* n,end=" ")
+    else:
+         print("*", end="")
+         print(" "* (n-2), end="")
+         print("*", end="")
+    print(" ")      
+
+
