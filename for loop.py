14d216b7b379a019ca69a564c1a44915d422645e
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-25 08:40:10 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,17 @@
+#for i in range(4):
+   # print(i)
+
+#l = [1,4,6,76,8,765]
+
+#for i in l:
+    #print(i)
+
+# t = (6,234,54,221)
+# for i in t:
+#     print(i)
+
+s = "Janvee"   #iterator for loop
+for i in s:
+    print(i)
+
+    
