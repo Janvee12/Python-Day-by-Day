0310360ee8ef53eb5b778a882c0c2fbccef976f2
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-26 12:47:10 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,15 @@
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add(3)
+collection.remove(1)
+#print(collection)
+
+val = {9,"9.0"}
+#print(val)
+
+value = {
+    ("float",9.0),
+    ("int",9)
+}
+print(value)
