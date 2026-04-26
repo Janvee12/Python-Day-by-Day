5104392553382e382b84c81e7c917cd4dc5d8847
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-26 12:46:36 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,20 @@
+e = set() #Dont use s = {} as it will create an empty dictionary
+
+s = {1,13,4,6,7,5}
+print(s)
+
+#s.add(45)
+#print(s,type(s)) #adding element 
+
+#print(len(s))
+#s. remove (1) #remove one element
+#print(s)
+# SET UNION INTERSECTION 
+s1 = {1,55,3}
+s2 = {55,9,25,1}
+
+#print(s1.union(s2))
+#print(s1.intersection(s2))
+print(s1.difference(s2))
+
+
