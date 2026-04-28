27b57a2059e95d69537478d7e890785b6d565680
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-28 10:24:58 +0530

    Add files via upload

---
+++ ques list.py
@@ -0,0 +1,26 @@
+#movies = []
+#mov1 = input("enter 1st movie:")
+#mov2 = input("enter 2nd movie:")
+#mov3 = input("enter 3rd movie:")
+
+#movies.append(mov1)
+#movies.append(mov2)
+#movies.append(mov3)
+#print(movies)
+
+
+#Palindrome
+
+#list1 = [1,2,1]
+#copy_list1 = list1.copy()
+#copy_list1.reverse()
+
+#if(copy_list1 == list1):
+ #   print("palidrome")
+#else:
+ #   print("not palindrame")
+
+grade = ["a" ,"d","a","c","d","a"]
+#print(grade.count("a"))
+grade.sort()
+print(grade)
