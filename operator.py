95aa8f92ed4f5b3cfd4c6533d107708676bbacd3
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-29 21:31:16 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,20 @@
+#Arithmatic operators
+a=2
+b=3
+c=a+b
+print(c)
+#Assignment operators
+a = 6-2
+print(a)
+b = 5
+b += 2
+print(b)
+#Camparsion Operators
+d = 6<3
+print(d)
+e = 6>3
+print(e)
+#Logical Operators
+
+e = False and True
+print(e)
