69076fb13a0c2d259f6ef4eef0dd37876b09d931
Janvee Sharma <jsharma22072004@gmail.com>
2026-04-30 15:45:23 +0530

    Add files via upload

---
+++ Practice dic1.py
@@ -0,0 +1,26 @@
+#words ={
+   #}
+#word = input("Enter the word you want meaning of: ")
+
+#print(words[word])
+
+
+s = set()
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+n = input("Enter number: ")
+s.add(int(n))
+
+print(s)
