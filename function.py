e3c0c1165392df59326eb46337e8ea22c7361eca
Janvee Sharma <jsharma22072004@gmail.com>
2026-05-01 18:13:17 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,24 @@
+# a = 12
+# b = 43
+# c = 25
+
+# average = (a + b + c)/3
+# print(average)
+
+#function definition
+# def avg():
+#     a = int(input("Enter your number: "))
+#     b = int(input("Enter your number: "))
+#     c = int(input("Enter your number: "))
+
+#     average = (a + b + c)/3
+#     print(average)
+
+# avg() #Function Call
+# avg()
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+
+goodDay("Harry", "Thank you")
+goodDay("Janvee", "Thank you")
