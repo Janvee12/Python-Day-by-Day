8033355622f73f162a10e756c35b5ca2500fb8bf
Janvee Sharma <jsharma22072004@gmail.com>
2026-05-07 12:57:04 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,16 @@
+class Employee:
+    language = "Py"
+    salary = 120000
+
+
+harry = Employee()
+harry.name = "Harry"
+print(harry.name, harry.language, harry.salary)
+
+rohan = Employee()
+rohan.name = "Rohan Roro"
+print(rohan.name, rohan.language, rohan.salary)
+
+#Here name is object attribute and salary and language are class 
+# attributes as they directly belong to the class
+
