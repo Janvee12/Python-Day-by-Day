e3df3f449b36dfd2ab1da364ccaf16b5272dd8cf
Janvee Sharma <jsharma22072004@gmail.com>
2026-05-12 19:02:45 +0530

    Add files via upload

---
+++ lecture three list&tuple.py
@@ -0,0 +1,13 @@
+marks =  [94,54,40,92,31]
+#print(marks)
+#print(type(marks))
+#print(len(marks))
+#print(marks[0:2])
+#marks.append(60)
+#print(marks)
+#print(marks.sort())
+#print(marks.sort(reverse=True))
+#print(marks)
+
+tup = (32,54,66,71)
+print(tup[0])
